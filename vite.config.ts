31acd6a6f67c480c6bdf58b24a7dd202c5881c11
mchnasy-glitch <mchnasy@gmail.com>
2026-09-03 10:32:12 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/nobateman/',
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -12,4 +13,5 @@ export default defineConfig({
     port: 3000,
   }
 })
+
 
